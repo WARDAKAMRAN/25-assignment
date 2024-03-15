adcd42539c512e072c6bd6933e16cc98f2e8be56
Warda Kamran <wkamran523@gmail.com>
2024-03-15 13:58:45 -0700

    25 assignment

---
+++ main.js
@@ -0,0 +1,10 @@
+// create a varibale called alien-color
+var alien_color = 'green';
+if (alien_color == "green") {
+    //console.log(" the player just earned 5 points.");  
+    //}
+    alien_color = 'red';
+    if (alien_color == "green") {
+        console.log(" the player just earned 5 points.");
+    }
+}
---
+++ main.ts
@@ -0,0 +1,13 @@
+// create a varibale called alien-color
+let alien_color: string = 'green';
+
+
+if (alien_color == "green"){
+    console.log(" the player just earned 5 points.");  
+}
+
+
+alien_color = 'red';
+if (alien_color == "green"){
+    console.log(" the player just earned 5 points.");  
+}
